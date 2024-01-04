2bd9f87642dbf82f4696d69c8db86282ecefb8bd
hadigghazi <128805519+hadigghazi@users.noreply.github.com>
2024-01-04 13:06:44 +0200

    initial commit

---
+++ lib/models/expense.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:date_format/date_format.dart';
+
+const uuid = Uuid();
+
+enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
+
+class Expense {
+  Expense(
+      {required this.title,
+      required this.amount,
+      required this.date,
+      required this.category})
+      : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  String get formattedDate {
+    String formatted = formatDate(
+        DateTime(date.year, date.month, date.day), [yyyy, '-', mm, '-', dd]);
+    return formatted;
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+}
